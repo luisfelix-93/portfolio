9266ee182a8fafdb19e7729d6a8b5129b8ce7c90
luisfelix-93 <lfff6493@gmail.com>
2023-10-31 08:33:54 -0300

    primeiro commit

---
+++ src/components/About/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './index.scss'
+const About = () => {
+  return (
+    <div className='container about-page'>
+        <div className='text-zone'>
+            <h1>
+                About me
+            </h1>
+            <p>
+                Formerly a mechanical engineer, passionate about technology, and currently a back-end developer.</p>
+            <p>
+                I am a mechanical engineer with a strong interest in technology. After years of working in the engineering field, I decided to make a career transition to software development. Today, I work as a back-end developer with technologies nodeJS, .NET c#, Python, and MySQL and SQL Server databases
+            </p>
+            <p>
+            In addition to my technical skills, I am also interested in DevOps. Recently, I started studying about the subject and I am excited to apply my knowledge to future projects.
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default About
